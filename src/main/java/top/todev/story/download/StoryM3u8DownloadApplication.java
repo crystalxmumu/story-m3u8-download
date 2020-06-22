491ea5ddf7f7e58ee5c12f44f5e2406f0a248bb6
hk <crystalxmumu@163.com>
2020-06-22 18:08:23 +0800

    项目初始化

---
+++ src/main/java/top/todev/story/download/StoryM3u8DownloadApplication.java
@@ -0,0 +1,13 @@
+package top.todev.story.download;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryM3u8DownloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoryM3u8DownloadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/todev/story/download/StoryM3u8DownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package top.todev.story.download;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryM3u8DownloadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
